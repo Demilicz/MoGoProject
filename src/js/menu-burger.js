81fe81fbec4b9bb7b209ec379abc4a3f09aefba8
Demilicz <Necrolick92@gmail.com>
2021-04-26 15:30:23 +0200

    add section about

--- src/js/menu-burger.js
+++ src/js/menu-burger.js
@@ -2,6 +2,10 @@ const iconMenu = document.querySelector('.menu-burger');
 const bodyMenu = document.querySelector('.header__list');
 const header = document.querySelector('.header');
 
+const searchBtn = document.querySelector(".header__search");
+const input = document.querySelector(".header__input");
+
+// console.log(input);
 
 // Menu-burger
 
@@ -18,47 +22,9 @@ iconMenu.addEventListener('click', () => {
   }
 });
 
-// Fixed Menu
-
-document.addEventListener('scroll', () => {
-  if(window.scrollY > 0) {
-
-    header.classList.add("shadow");
- }
- else
-    header.classList.remove("shadow");
-
+searchBtn.addEventListener('click', () => {
+  input.classList.toggle('d-none');
 });
 
 
 
-// navigation
-
-const menuLinks = document.querySelectorAll('.header__item');
-
-if(menuLinks.length > 0){
-  menuLinks.forEach(link => {
-    link.addEventListener('click', onMenuLinkClick)
-  });
-}
-
-function onMenuLinkClick(e) {
-  const menuLink = e.target;
-  if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-    const gotoBlock = document.querySelector(menuLink.dataset.goto);
-    const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
-
-    if(iconMenu.classList.contains('_active')){
-      document.body.classList.remove('_lock');
-      iconMenu.classList.remove('_active');
-      bodyMenu.classList.remove('_active');
-      document.body.style.overflow = "scroll";
-    }
-
-    window.scrollTo({
-      top: gotoBlockValue,
-      behavior: "smooth"
-    });
-    e.preventDefault();
-  }
-}
